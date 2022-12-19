8ffd0c5322ccd4d26b32616d85361fd99f1c6a18
Emad Khan <emadkhanqai@gmail.com>
2022-12-19 18:58:56 +0500

    changed channel name prop to observable

--- src/app/custom-counter-input/custom-counter-input.component.ts
+++ src/app/custom-counter-input/custom-counter-input.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
 import { changeChannelName, customIncrement } from '../state/counter.actions';
 import { getChannelName, getCounter } from '../state/counter.selector';
 import { CounterState } from '../state/counter.state';
@@ -11,12 +12,12 @@ import { CounterState } from '../state/counter.state';
 })
 export class CustomCounterInputComponent implements OnInit {
   value!: number;
-  channelName!: string;
+  channelName$!: Observable<string>;
 
   constructor(private store: Store<{ counter: CounterState }>) { }
 
   ngOnInit(): void {
-    this.store.select(getChannelName).subscribe((data: any) => this.channelName = data);
+    this.channelName$ = this.store.select(getChannelName);
   }
 
   changeName() {
@@ -24,6 +25,6 @@ export class CustomCounterInputComponent implements OnInit {
   }
 
   onAdd() {
-    this.store.dispatch(customIncrement({ value: this.value }));
+    this.store.dispatch(customIncrement({ value: +this.value }));
   }
 }
