bcab5f18accd0d544c9bf932bf30bda8025daf5e
Emad Khan <emadkhanqai@gmail.com>
2022-12-23 19:22:53 +0500

    Fixing the logout bug to prevent multiple HTTP API Calls

--- src/app/services/auth-token.interceptor.ts
+++ src/app/services/auth-token.interceptor.ts
@@ -5,7 +5,7 @@ import {
   HttpEvent,
   HttpInterceptor
 } from '@angular/common/http';
-import { exhaustMap, Observable } from 'rxjs';
+import { exhaustMap, Observable, take } from 'rxjs';
 import { Store } from '@ngrx/store';
 import { AppState } from '../store/app.state';
 import { getToken } from '../auth/state/auth.selector';
@@ -17,6 +17,7 @@ export class AuthTokenInterceptor implements HttpInterceptor {
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     return this.store.select(getToken).pipe(
+      take(1),
       exhaustMap((token: any) => {
         if (!token) {
           return next.handle(request);
